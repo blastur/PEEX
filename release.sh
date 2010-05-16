a92b6067f321940a11b95894965e85930b4c31f9
Magnus Olsson <magnus@minimum.se>
2010-05-16 22:43:58 +0200

    add: basic release-script

---
+++ release.sh
@@ -0,0 +1,10 @@
+#/bin/sh
+NAME=peex-1.0
+
+echo "Building $NAME"
+rm -rf $NAME
+mkdir -p $NAME
+cp peex ops.py tree.py $NAME/
+cp README example.peex $NAME/
+tar -cf $NAME.tar $NAME/
+gzip $NAME.tar
