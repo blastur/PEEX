528fdb6c5ab4d30796339c5c91abeb28024f184a
Magnus Olsson <magnus@minimum.se>
2010-05-18 20:11:50 +0200

    add: defaults.peex (dictates default values for missing config directives) mod: include defaults.peex in releases

--- release.sh
+++ release.sh
@@ -4,7 +4,7 @@ NAME=peex-1.0
 echo "Building $NAME"
 rm -rf $NAME
 mkdir -p $NAME
-cp peex ops.py tree.py $NAME/
+cp peex defaults.peex ops.py tree.py $NAME/
 cp README example.peex $NAME/
 tar -cf $NAME.tar $NAME/
 gzip $NAME.tar
